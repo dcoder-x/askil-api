603a167b2eacf1e315396d59010af3e7228dea04
dcoder-x <ayomikunfaluyi@gmail.com>
2023-09-20 21:57:12 +0100

    updated user controller

--- controllers/webhook.js
+++ controllers/webhook.js
@@ -1,25 +1,30 @@
 const Users = require("../models/UsersModel");
 
+exports.webhook = async (req, res) => {
+  const { id, customer } = req.body;
 
+  const { email } = customer;
+  console.log(id);
 
-
-
-exports.webhook = async (req,res)=>{
-    const {id,customer} = req.body
-
-    const {email}= customer
-    console.log(id)
-
-    try {
-        const user = new Users({
-           user_id: String(id),
-           order_email:email,
-           user_url:`https://askil.onrender.com/view_profile/${id}`
-          });
-          user.save(user)
-          res.status(200).json({msg:'recieved webhook',url:`https://askil.onrender.com/view_profile/${id}`})
-    } catch (error) {
-        res.status(400).json({msg:'rejected webhook'})
+  try {
+    const idExists = await Users.findOne({ user_id: id });
+    if (idExists) {
+      res.status(400).json({ msg: "Id has been used, purchase another card" });
+    } else {
+      const user = new Users({
+        user_id: String(id),
+        order_email: email,
+        user_url: `https://askil.onrender.com/view_profile/${id}`,
+      });
+      user.save(user);
+      res
+        .status(200)
+        .json({
+          msg: "recieved webhook",
+          url: `https://askil.onrender.com/view_profile/${id}`,
+        });
     }
-    
-}
+  } catch (error) {
+    res.status(400).json({ msg: "rejected webhook" });
+  }
+};
